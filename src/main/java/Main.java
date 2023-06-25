fc3a92f4f7f7595c0c24451b93fd4fd0b1b9acf0
ekowibowo-prog <ekowibowo309079@gmail.com>
2023-06-25 13:02:27 +0700

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Main {
+     public static void main(String[] args) {
+        Form_Utama form = new Form_Utama();
+        form.setVisible(true);
+      
+    }
+}
